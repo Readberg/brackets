bc3cf87dc276b7f9be0a57e801dd837ba1b63703
readberg <wayreadberg@gmail.com>
2023-01-29 08:46:22 +0300

    feat: add decide

--- src/index.js
+++ src/index.js
@@ -1,32 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  const OPEN_BRACKETS = ['(', '{', '['];
-  const BRACKETS_PAIR = {
-    [')']: '(',
-    ['}']: '{',
-    [']']: '[',
-  }
-    let stack = [];
+  let bracketStack = [];
 
-    for (let i = 0; i < str.length; i++) {
-      let currentSymbol = str[i];
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
 
-      if (OPEN_BRACKETS.includes(currentSymbol)) {
-        stack.push(currentSymbol)
-      } else {
-        if (stack.length === 0) {
-          return false;
-        }
+    if (bracketsConfig.some(config => config[0] === char)) {
+      bracketStack.push(char);
+    } else {
+      let lastBracket = bracketStack.pop();
 
-        let topElement = stack[stack.length - 1];
-
-        if (BRACKETS_PAIR[currentSymbol] === topElement) {
-          stack.pop();
-        } else {
-          return false
-        }
+      if (!bracketsConfig.some(config => config[0] === lastBracket && config[1] === char)) {
+        return false;
       }
     }
+  }
 
-    return true;
+  return bracketStack.length === 0;  
 }
